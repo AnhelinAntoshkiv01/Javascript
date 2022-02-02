85201d5c79b70ba441afea0b7aaaec0f875b1322
AnhelinAntoshkiv01 <antoshkivangel@gmail.com>
2022-02-02 16:14:35 +0200

    change v in cw.js

--- lesson 1/cw.js
+++ lesson 1/cw.js
@@ -1,7 +1,7 @@
 //           0  1  2  3  4  5  6  7  8  9
 let number =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(number);
-let result =(number[0]+number[1]+number[2]+number[3]+number[4]+number[5]+number[6]+number[7]+number[8]+number[9]);
+let result = number[0]+number[1]+number[2]+number[3]+number[4]+number[5]+number[6]+number[7]+number[8]+number[9];
 console.log(result);
 
 
@@ -39,20 +39,21 @@ console.log(product);
 
 let height = 23;
 let width = 10;
-let s =(height*width);
+let s = height*width;
 console.log(s);
 
 
 
 let heightC = 10;
 let dC = 4;
-let v =(heightC*dC);
+let pi = 3.14159265359;
+let v = pi * Math.pow(dC / 2, 2) * heightC;
 console.log(v);
 
 
 
 let n = 3;
 let m = 4;
-let k =(Math.sqrt(Math.pow(n,2)+Math.pow(m,2)))
+let k = Math.sqrt(Math.pow(n,2)+Math.pow(m,2))
 console.log(k)
 
