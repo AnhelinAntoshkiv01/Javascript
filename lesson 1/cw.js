885956b3f4a541061d9e624b6f3de6b9178ea45f
AnhelinAntoshkiv01 <antoshkivangel@gmail.com>
2022-02-02 16:58:01 +0200

    add task in js

--- lesson 1/cw.js
+++ lesson 1/cw.js
@@ -1,3 +1,7 @@
+let task1 = `Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor).
+Скласти результат цих чисел в змінній`;
+console.log(task1);
+
 //           0  1  2  3  4  5  6  7  8  9
 let number =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(number);
@@ -5,7 +9,8 @@ let result = number[0]+number[1]+number[2]+number[3]+number[4]+number[5]+number[
 console.log(result);
 
 
-
+let task2 = `Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр`;
+console.log(task2);
 let book ={
     title: 'Cat',
     quantity: 15,
@@ -14,6 +19,8 @@ let book ={
 console.log(book);
 
 
+let task3 = `Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори`;
+console.log(task3);
 let book2 ={
     title: 'Cinderella',
     quantity: 15,
@@ -24,6 +31,8 @@ console.log(book2);
 
 
 
+let task4 = `Створити масив об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт`;
+console.log(task4);
 let book3 ={
     title: 'Donkey Skin',
     quantity: 15,
@@ -37,13 +46,18 @@ console.log(product);
 
 
 
+let task5 = `Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+   Значення площі зберігати в змінній s.`;
+console.log(task5);
 let height = 23;
 let width = 10;
 let s = height*width;
 console.log(s);
 
 
-
+let task6 = `Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+        результат помістіть у змінну v.`;
+console.log(task6);
 let heightC = 10;
 let dC = 4;
 let pi = 3.14159265359;
@@ -51,7 +65,9 @@ let v = pi * Math.pow(dC / 2, 2) * heightC;
 console.log(v);
 
 
-
+let task7 = `У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+       Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).`;
+console.log(task7);
 let n = 3;
 let m = 4;
 let k = Math.sqrt(Math.pow(n,2)+Math.pow(m,2))
--- lesson 1/hm.js
+++ lesson 1/hm.js
@@ -1,3 +1,6 @@
+let hm1 = `Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+  Вивести кожну змінну за допомогою: console.log , alert, document.write`;
+console.log(hm1);
 let one = `hello`;
 let two = `owu`;
 let three = `com`;
@@ -53,6 +56,9 @@ console.log(xxx);
 
 
 
+let hm2 = `Переприсвоїти кожній змінній з завдання значення на довільне.
+  Вивести кожну змінну за допомогою: console.log , alert, document.write`;
+console.log(hm2);
 one = 5;
 two = 8;
 three = 3;
@@ -71,14 +77,19 @@ console.log(four);
 // document.write(`<div>${four}</div>`);
 
 
+let hm3 = `Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)`;
+console.log(hm3);
 let firstName = `Anhelina`;
 let middleName = `Volodimirivna`;
 let lastName = `Antoshkiv`;
-let person = [firstName, middleName, lastName];
+let person = (`${firstName}, ${middleName}, ${lastName}`);
 console.log(person);
 
 
 
+let hm4 = `За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+    Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".`;
+console.log(hm4);
 let hi = `Hello Anhelina?`;
 let hi2 = `Hello Anhelina Volodimirivna?`;
 let hi3 = `Are you 21 years old?`;
@@ -87,6 +98,9 @@ prompt(hi2);
 prompt(hi3);
 
 
+let hm5 = `За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+  let a = 100; let b = '100'; let c = true;`;
+console.log(hm5);
 let a = 100;
 let b = `100`;
 let c = true;
@@ -95,6 +109,21 @@ console.log(typeof b);
 console.log(typeof c);
 
 
+
+let hm6 = `Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+В однакових виразах не використовувати однакові оператори!!!
+  5 ? 6 -> true
+  5 ? 6 -> false
+  5 ? 6 -> false
+  5 ? 6 -> false
+  10 ? 10 -> true
+  10 ? 10 -> true
+  10 ? 10 -> false
+  10 ? 10 -> false
+  10 ? 10 -> false
+  123 ? '123' -> false
+  123 ? '123' -> true`;
+console.log(hm6);
 console.log(5<6);
 console.log(5>6);
 console.log(5>=6);
@@ -108,12 +137,33 @@ console.log(123<`123`);
 console.log(123!==`123`);
 
 
+
+let hm7 = `Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+    let str = "20";
+     let a = 5;
+     document.write(str + a + "<br/>");
+     document.write(str - a + "<br/>");
+     document.write(str * "2" + "<br/>");
+     document.write(str / 2 + "<br/>");`;
+console.log(hm7);
 let str = "20";
 let y = 5;
 document.write(`<div>${str + y + "<br/>"}</div>`);
+console.log(str + y + "<br/>");
+let w = `результат 205, так як 20 - стрічковий елемент,а 5 число`;
+console.log(w);
 document.write(`<div>${str - y + "<br/>"}</div>`);
+console.log(str - y + "<br/>");
+let q = `результат 15, так як 20 - стрічковий елемент при множенні, діленні і відніманні переводиться у число`;
+console.log(q);
 document.write(`<div>${str * "2" + "<br/>"}</div>`);
+console.log(str * "2" + "<br/>");
+let t = `результат 40, так як 20 - стрічковий елемент при множенні, діленні і відніманні переводиться у число`;
+console.log(t);
 document.write(`<div>${str / 2 + "<br/>"}</div>`);
+console.log(str / 2 + "<br/>");
+let p = `результат 10, так як 20 - стрічковий елемент при множенні, діленні і відніманні переводиться у число`;
+console.log(p);
 
 
 
