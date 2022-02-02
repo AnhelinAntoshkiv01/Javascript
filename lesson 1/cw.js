084e2ef7fefb57f55c8d736d6dbb5f10d085aecc
AnhelinAntoshkiv01 <antoshkivangel@gmail.com>
2022-02-02 16:06:14 +0200

    Initial commit

---
+++ lesson 1/cw.js
@@ -0,0 +1,58 @@
+//           0  1  2  3  4  5  6  7  8  9
+let number =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(number);
+let result =(number[0]+number[1]+number[2]+number[3]+number[4]+number[5]+number[6]+number[7]+number[8]+number[9]);
+console.log(result);
+
+
+
+let book ={
+    title: 'Cat',
+    quantity: 15,
+    genre: 'tale'
+}
+console.log(book);
+
+
+let book2 ={
+    title: 'Cinderella',
+    quantity: 15,
+    genre: 'tale',
+    author: 'brothers Grimm'
+}
+console.log(book2);
+
+
+
+let book3 ={
+    title: 'Donkey Skin',
+    quantity: 15,
+    genre: 'tale',
+    author: 'brothers Grimm'
+}
+let product = [book3, book2];
+console.log(book3);
+console.log(book2);
+console.log(product);
+
+
+
+let height = 23;
+let width = 10;
+let s =(height*width);
+console.log(s);
+
+
+
+let heightC = 10;
+let dC = 4;
+let v =(heightC*dC);
+console.log(v);
+
+
+
+let n = 3;
+let m = 4;
+let k =(Math.sqrt(Math.pow(n,2)+Math.pow(m,2)))
+console.log(k)
+
---
+++ lesson 1/hm.js
@@ -0,0 +1,119 @@
+let one = `hello`;
+let two = `owu`;
+let three = `com`;
+let four = `ua`;
+let five = 1;
+let six = 10;
+let seven = -999;
+let eight = 123;
+let PI = 3.14;
+let nine = 2.7;
+let ten = 16;
+let yyy = true;
+let xxx = false;
+console.log(one);
+// alert(one);
+// document.write(`<div>${one}</div>`);
+console.log(two);
+// alert(two);
+// document.write(`<div>${two}</div>`);
+console.log(three);
+// alert(three);
+// document.write(`<div>${three}</div>`);
+console.log(four);
+// alert(four);
+// document.write(`<div>${four}</div>`);
+console.log(five);
+// alert(five);
+// document.write(`<div>${five}</div>`);
+console.log(six);
+// alert(six);
+// document.write(`<div>${six}</div>`);
+console.log(seven);
+// alert(seven);
+// document.write(`<div>${seven}</div>`);
+console.log(eight);
+// alert(eight);
+// document.write(`<div>${eight}</div>`);
+console.log(PI);
+// alert(PI);
+// document.write(`<div>${PI}</div>`);
+console.log(nine);
+// alert(nine);
+// document.write(`<div>${nine}</div>`);
+console.log(ten);
+// alert(ten);
+// document.write(`<div>${ten}</div>`);
+console.log(yyy);
+// alert(yyy);
+// document.write(`<div>${yyy}</div>`);
+console.log(xxx);
+// alert(xxx);
+// document.write(`<div>${xxx}</div>`);
+
+
+
+one = 5;
+two = 8;
+three = 3;
+four = 7;
+console.log(one);
+// alert(one);
+// document.write(`<div>${one}</div>`);
+console.log(two);
+// alert(two);
+// document.write(`<div>${two}</div>`);
+console.log(three);
+// alert(three);
+// document.write(`<div>${three}</div>`);
+console.log(four);
+// alert(four);
+// document.write(`<div>${four}</div>`);
+
+
+let firstName = `Anhelina`;
+let middleName = `Volodimirivna`;
+let lastName = `Antoshkiv`;
+let person = [firstName, middleName, lastName];
+console.log(person);
+
+
+
+let hi = `Hello Anhelina?`;
+let hi2 = `Hello Anhelina Volodimirivna?`;
+let hi3 = `Are you 21 years old?`;
+prompt(hi);
+prompt(hi2);
+prompt(hi3);
+
+
+let a = 100;
+let b = `100`;
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+console.log(5<6);
+console.log(5>6);
+console.log(5>=6);
+console.log(5===6);
+console.log(10===10);
+console.log(10==10);
+console.log(10<10);
+console.log(10>10);
+console.log(10!==10);
+console.log(123<`123`);
+console.log(123!==`123`);
+
+
+let str = "20";
+let y = 5;
+document.write(`<div>${str + y + "<br/>"}</div>`);
+document.write(`<div>${str - y + "<br/>"}</div>`);
+document.write(`<div>${str * "2" + "<br/>"}</div>`);
+document.write(`<div>${str / 2 + "<br/>"}</div>`);
+
+
+
