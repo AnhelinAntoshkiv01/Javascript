4c9adefa3a50ff7101408d901f1b1ca8fa794126
AnhelinAntoshkiv01 <antoshkivangel@gmail.com>
2022-02-09 16:16:41 +0200

    add lesson 4

---
+++ lesson 4/cw.js
@@ -0,0 +1,140 @@
+console.log(`створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)`);
+
+function numMin(num1, num2, num3){
+    if (num1 < num2 && num3){
+        console.log(num1);
+    } else if (num2 < num1 && num3){
+        console.log(num2);
+    } else {
+        console.log(num3);
+    }
+}
+numMin(5, 8, 6);
+
+
+
+console.log(`створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)`);
+
+function numMax(num1, num2, num3){
+    if (num1 > num2 && num3){
+        console.log(num1);
+    } else if (num2 > num3 && num2){
+        console.log(num2);
+    } else {
+        console.log(num3);
+    }
+}
+numMax(8, 8, 6);
+
+
+
+console.log(`створити функцію яка повертає найбільше число з масиву`);
+
+function arrayMax(arr){
+    let max = arr[0];
+    for (const item of arr) {
+        if (item > max){
+            max = item;
+        }
+    }
+    return max;
+}
+let array = [123, 56, 89];
+console.log(arrayMax(array));
+
+
+
+console.log(`створити функцію яка повертає найменьше число з масиву`);
+
+function arrayMin(arr){
+    let min = arr[0];
+    for (const item of arr) {
+        if (item < min){
+            min = item;
+        }
+    }
+    return min;
+}
+console.log(arrayMin(array));
+
+
+
+console.log(`створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13`);
+
+function sum(arr){
+    let result = 0;
+    for (const arrElement of arr) {
+        result = result += arrElement;
+    }
+    return result;
+}
+console.log(sum(array));
+
+
+
+console.log(`створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.`);
+
+function average(nums) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
+}
+console.log(average(array));
+
+
+
+console.log(`створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено)`);
+
+function result(fg) {
+    let max = arguments[0];
+    let min = arguments[0];
+    for (const element of arguments) {
+        if (element > max){
+            max = element;
+        }
+        if (element < min){
+            min = element;
+        }
+    }
+    console.log(max);
+    return min;
+}
+console.log(result(5, 56, 87, 123));
+
+
+
+console.log(`створити функцію яка заповнює масив рандомними числами
+(цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.`);
+
+function random(length, limit){
+    let array = [];
+    for (let i = 0; i < length; i++) {
+        array.push(Math.round(Math.random()*100))
+    }
+    return array;
+}
+console.log(random(15, 100));
+
+
+
+console.log(`створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.`);
+
+function limit(length, limit){
+    let array = [];
+    for (let i=0; i<length; i++) {
+        array.push(Math.round(Math.random()*limit))
+    }
+    return array;
+}
+console.log(limit(11, 55));
+
+
+
+console.log(`Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].`);
+
+function reverse(arr){
+    let a = [];
+    for (j=0; j<arr.length; j++){
+        a[j] = arr[(arr.length - 1) - j]
+    }
+    return a;
+}
+console.log(reverse([1, 6, 8]));
---
+++ lesson 4/hm.js
@@ -0,0 +1,111 @@
+console.log(`створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б`);
+
+function rectangle(a, b){
+    let S = a*b;
+    return S;
+}
+console.log(rectangle(10,15));
+
+
+
+console.log(`створити функцію яка обчислює та повертає площу кола з радіусом r`);
+
+function round(r){
+    const PI = 3.14;
+    let s = PI*(r**2);
+    return s;
+}
+console.log(round(5));
+
+
+
+console.log(`створити функцію яка обчислює та повертає площу циліндру висотою h, та радіусом r`);
+
+function cylinder(h, r){
+    const PI = 3.14;
+    let s = 2*PI*r*h;
+    return s;
+}
+console.log(cylinder(15, 3));
+
+
+
+console.log(`створити функцію яка приймає масив та виводить кожен його елемент`);
+
+function dataArray(arr){
+    for (let i = 0; i < arr.length; i++) {
+        const arrElement = arr[i];
+        console.log(arr[i]);
+    }
+}
+dataArray([1, 5, `fly`, true, 8, `go`]);
+
+
+
+console.log(`створити функцію яка створює параграф з текстом. Текст задати через аргумент`);
+
+function text(p){
+    document.write(`<p>${p}</p>`);
+    document.write(`<hr>`);
+    document.write(`<p>${p}</p>`);
+    for (let i = 0; i < 5; i++) {
+        document.write(`<p>${p}</p>`);
+    }
+    document.write(`<hr>`);
+}
+text(`Do it!`);
+
+
+
+console.log(`створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий`);
+
+function ul(li){
+    document.write(`<ul>`);
+    document.write(`<li>${li}</li>`);
+    document.write(`<li>${li}</li>`);
+    document.write(`<li>${li}</li>`);
+    document.write(`</ul>`);
+    document.write(`<hr>`);
+}
+ul(`It's Better.`);
+
+
+
+console.log(`створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)`);
+
+function ul2(li, length){
+    document.write(`<ul>`);
+    for (let i = 0; i < length; i++) {
+        document.write(`<li>${li}</li>`);
+    }
+    document.write(`</ul>`);
+    document.write(`<hr>`);
+}
+ul2(`It's Better.`, 5);
+
+
+
+console.log(`створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список`);
+
+function list(arr){
+    document.write(`<ol>`);
+    for (let i = 0; i < arr.length; i++) {
+        document.write(`<li>${arr[i]}</li>`);
+    }
+    document.write(`</ol>`);
+}
+list([1, `cool`, true]);
+
+
+
+console.log(`створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.`);
+
+function info(users){
+    for (const user of users) {
+        document.write(`<div>${user.id} ${user.name} ${user.age}</div>`);
+    }
+}
+info( [
+    {id: 5435465, name: `Eva`, age: 24},
+    {id: 5736587, name: `Jane`, age: 21},
+    {id: 5876923, name: `Den`, age: 28}]);
